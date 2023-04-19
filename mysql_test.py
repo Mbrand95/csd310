18736ade709d8b1e305673b67663447613736abe
Mbrand95 <Micheleabrand95@gmail.com>
2023-04-18 21:40:59 -0400

    4-18

---
+++ mysql_test.py
@@ -0,0 +1,27 @@
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user" : "movies_user",
+    "password": "popcorn",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database User {} connected to MYSQL on host {} with database {}", format(config["user"]), config["host"], config["database"])
+    input("\n\n press any key to continue. . .")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Username or password is incorrect")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+finally:
+    db.close()
